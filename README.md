8885704c63fc575fdcf1fab8f228e7e2d059546d
Steven Antonio <steven@stevenanton.io>
2017-07-30 17:06:27 -0700

    basic smtp mailer

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Backend for Teamname by Proxy site
 
-### AWS ambda entpoint
-
+### AWS ambda entpoints
+ROOT
 https://9374efz9mk.execute-api.us-west-1.amazonaws.com/dev/
+EXCEPTION FORM
+https://9374efz9mk.execute-api.us-west-1.amazonaws.com/dev/sendex
 
 ### Licence
 GNU GENERAL PUBLIC LICENSE
--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from chalice import Chalice
-from services.messanger import sendtext
-from services.messanger import sendmail
+from datetime import datetime
+import smtplib
+
+# APP_EMAIL = 'exceptionapplication@gmail.com'
+# DEFUALT_RESPONSE='Exception sent'
 
 app = Chalice(app_name='teamsite-chalice')
 
@@ -8,6 +11,65 @@ app = Chalice(app_name='teamsite-chalice')
 @app.route('/')
 def index():
     return {'hello': 'world'}
+
+@app.route('/sendex', methods=['POST'])
+def sendexception(agent='Bob', supervisor="Tyler"):
+    gmail_user = 'exappv2@gmail.com'
+    gmail_password = '!Qaz#Edc'
+    sent_from = gmail_user
+    to = 'pastrana.steven.az@gmail.com'
+    subject = 'Fixing'
+    body = "Hey, what's up?"
+
+    agent = agent
+    supervisor = supervisor
+
+    form = """
+    Exception form
+    ===============
+
+    Agent: %s
+    Supervisor: %s
+    """ % (agent, supervisor)
+
+    message = """From: %s
+    To: %s
+    MIME-Version: 1.0
+    Content-type: text/html
+    Subject: %s
+
+    %s
+
+    """ % (sent_from, to, subject, form)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, message)
+        server.close()
+        return {'success': 'Yay!!'}
+    except:
+        return {'error': 'happened'}
+
+# @app.route('/exform', methods=['POST'])
+# def send_mail(sender, sender_email, manager, approved_by, exception_date, start_time, end_time, reason="LEFT EMPTY", recipient="steven.antonio.dev@gmail.com", when=datetime.now()):
+#     yag = yagmail.SMTP('exceptionapplication', '!Qaz#Edc')
+#     title = '<h2>Exception Submission</h2>'
+#     time_sent = '<p><b>Time Sent: </b>{}</p>'.format(when)
+#     sender = '<p><b>Agent: </b>{}</p>'.format(sender)
+#     sender_email = '<p><b>Agent Email: </b>{}</p>'.format(sender_email)
+#     manager = '<p><b>Manager: </b>{}</p>'.format(manager)
+#     exception_date = '<p><b>Date: <b>{}</p>'.format(exception_date)
+#     start_time = '<p><b>Start Time: </b>{}</p>'.format(start_time)
+#     end_time = '<p><b>End Time: </b>{}</p>'.format(end_time)
+#     reason = '<p><b>Reason: </b>{}</p>'.format(reason)
+#     email_footer = '<br><p>=====================================</p></br><p>This email was automagically created and sent to {} using python 3, yag, and some coding judo.'.format(recipient)
+#     contents = [title, time_sent, sender, sender_email, manager, exception_date, start_time, end_time, reason, email_footer]
+#     # Send the email to the manager | Change the email in the functiion to the variable recipient
+#     yag.send('pastrana.steven.az@gmail.com', 'Exception Submission', contents)
+#     # Send the confirmation email
+#     yag.send(sender, 'Exception Confirmation', 'Your exception was successfuly submited.')
 
 
 # The view function above will return {"hello": "world"}
